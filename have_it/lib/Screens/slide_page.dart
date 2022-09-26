9c66e0f2cd25f4cae882cd8e014a8691efdef1de
ADIBA NOOR <adiba.noor003@gmail.com>
2022-09-26 21:32:23 +0530

    fixed one error

--- have_it/lib/Screens/slide_page.dart
+++ have_it/lib/Screens/slide_page.dart
@@ -14,28 +14,31 @@ class SlidePage extends StatefulWidget {
 class _SlidePageState extends State<SlidePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: ftcolor,
-      padding: EdgeInsets.symmetric(horizontal: 25),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Welcome!!!",style: TextStyle(color: fscolor,fontSize: 32,fontWeight: FontWeight.bold),),
-            SizedBox(height: 20,),
-            SlideAction(
-              borderRadius: 12,
-              outerColor: sccolor,
-              innerColor: ficolor,
-              text: "Slide To Proceed",
-              sliderButtonIcon: Icon(Icons.chevron_right),
-              onSubmit: (){
-               Navigator.push(context, MaterialPageRoute(builder: (context)=> Home()));
-              },
-              sliderRotate: false,
+    return Scaffold(
+      body: Container(
+        color: ftcolor,
+        padding: EdgeInsets.symmetric(horizontal: 25),
+        child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Welcome!!!",style: TextStyle(color: fscolor,fontSize: 32,fontWeight: FontWeight.bold),),
+                SizedBox(height: 25,),
+                SlideAction(
+                  borderRadius: 12,
+                  outerColor: sccolor,
+                  innerColor: ficolor,
+                  text: " Slide To Proceed",
+                  sliderButtonIcon: Icon(Icons.chevron_right),
+                  onSubmit: (){
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> Home()));
+                  },
+                  sliderRotate: false,
+
+                )
+              ],
             )
-          ],
-        )
+        ),
       ),
     );
   }
